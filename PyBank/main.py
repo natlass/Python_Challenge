8db90c0fb4d070f56d4ca32a581f1fa5664e52b2
nlass <natlassen@gmail.com>
2020-10-10 18:16:37 -0400

    created tally of each candidate

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,18 +3,14 @@ import os
 import csv
 
 #Set csv resource location
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csv_path = os.path.join('Resources/budget_data.csv')
 
 #Open with improved reading
-with open(csvpath, 'r') as csvfile:
-    # CSV reader specifies delimiter and variable
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-    print(csvreader)
-
-    #Read the header row first
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+with open(csv_path, 'r', newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    #skip header
+    if csv.Sniffer().has_header:
+        next(csvreader)
 
     #Read each row of data after the header
     for row in csvreader:
---
+++ PyPoll/main.py
@@ -0,0 +1,40 @@
+#Import
+import os
+import csv
+
+#set csv path
+csv_path = os.path.join("Resources", "election_data.csv")
+
+#open reader
+with open(csv_path, 'r', newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    #skip header
+    if csv.Sniffer().has_header:
+        next(csvreader)
+
+    #set variables
+    voter_id = 0
+    candidate = 0
+
+    #set dictionary
+    election = {}
+ 
+    #create list of candidates
+    candidate_list = []
+
+    #Print the data in the rows after the header
+    for row in csvreader:
+        
+        #tally of first column, omitting header still
+        voter_id = voter_id + 1
+
+        #append to empty list as you go
+        if (row[2]) not in candidate_list:
+            candidate_list.append(row[2])
+            election[row[2]] = 0
+
+        election[row[2]] = election[row[2]] + 1
+
+print(election)
+        
+        
